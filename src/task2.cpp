6169460a3df9834aaa77a3db0a3cb45cdda7aa11
eliyarabia <eliyarabia@gmail.com>
2024-01-13 14:53:01 +0200

    ypdate

--- src/task2.cpp
+++ src/task2.cpp
@@ -2,23 +2,8 @@
 #include <string>
 using namespace std;
 
-bool isCustomURL(string input) {
-    // Check if the input starts with "www." and ends with ".com"
-    return (input.compare(0, 4, "www.") == 0 && input.length() > 8 &&
-            input.compare(input.length() - 4, 4, ".com") == 0);
-}
-
-unsigned long hashDJB2(const std::string& str) {
-    unsigned long hash = 5381;
-
-    for (char c : str) {
-        hash = ((hash << 5) + hash) + c; // hash * 33 + c
-    }
-
-    return hash;
-} 
-
 int main() {
+    
     string sort[200];
     hash<string> myhash;
     string hashing;
@@ -38,21 +23,13 @@ int main() {
                 
             }
         }
-         
-        
-    }
-        
-
-
-        // as you saw, if you inputted a line for the first input,
+         // as you saw, if you inputted a line for the first input,
 	    // then it took the first word as the first input,
 	    // and the rest of the line as the sccond input
 	    // to fix this, we will clear the input buffer
 	    cin.clear();
-    
-
-   
-
-
+        
+    }
+        
 	return 0;
 }
