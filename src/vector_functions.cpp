79b446435d326ea3ea6b445116e489c81779c477
orshmuel9 <orshmuel9@gmail.com>
2024-01-14 09:10:19 +0200

    vector change

--- src/vector_functions.cpp
+++ src/vector_functions.cpp
@@ -12,18 +12,18 @@ bool checkIfUrlExist(vector<string> urls, string new_url){
     return false;
 }
 
-int main ()   
-{   
-    vector<string> check;
-    check.push_back("10");
-    check.push_back("20");
-    check.push_back("30");
-    check.push_back("30");
-    check.push_back("30");
-    for (string i : check) {
-        cout << i << " " << endl;
-    }
-    cout << checkIfUrlExist(check,"10") << endl;
-    cout << checkIfUrlExist(check,"100") << endl;
-    return 0;  
-}  
+// int main ()   
+// {   
+//     vector<string> check;
+//     check.push_back("10");
+//     check.push_back("20");
+//     check.push_back("30");
+//     check.push_back("30");
+//     check.push_back("30");
+//     for (string i : check) {
+//         cout << i << " " << endl;
+//     }
+//     cout << checkIfUrlExist(check,"10") << endl;
+//     cout << checkIfUrlExist(check,"100") << endl;
+//     return 0;  
+// }  
